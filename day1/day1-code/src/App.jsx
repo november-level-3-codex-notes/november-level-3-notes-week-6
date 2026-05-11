cde2190a87ced3b3490ef9ce0478b895d2cc2acb
Thomas Coons <tomcoons1413@gmail.com>
2026-05-11 15:14:34 -0400

    day1 starter code added

--- day1/day1-code/src/App.jsx
+++ day1/day1-code/src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <div>App</div>;
 }
 
 export default App;
---
+++ day1/day1-code/src/components/MovieTable.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MovieTable.css";
+
+function MovieTable() {
+  return <div>MovieTable</div>;
+}
+
+export default MovieTable;
