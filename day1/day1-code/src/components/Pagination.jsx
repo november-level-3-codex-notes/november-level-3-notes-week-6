e9600e3e6e52e0feb1dd089eb324a8b762a66667
Thomas Coons <tomcoons1413@gmail.com>
2026-05-11 21:24:11 -0400

    completed day1 code

--- day1/day1-code/src/components/Pagination.jsx
+++ day1/day1-code/src/components/Pagination.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Pagination.css";
 
-function Pagination() {
+function Pagination({ setPage, page, numberOfPages }) {
   return (
-    <div>Pagination</div>
-  )
+    <div className="pagination">
+      <button disabled={page === 0} onClick={() => setPage((prev) => prev - 1)}>
+        Prev
+      </button>
+      <button
+        disabled={page === numberOfPages}
+        onClick={() => setPage((prev) => prev + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
