44cc990ffcd125f777a93a6f6af5ecbacd2ea285
Thomas Coons <tomcoons1413@gmail.com>
2026-05-12 13:42:01 -0400

    components added to day2

---
+++ day2/day2-code/src/components/AddMovieForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddMovieForm() {
+  return <div>AddMovieForm</div>;
+}
+
+export default AddMovieForm;
---
+++ day2/day2-code/src/components/AuthForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AuthForm.css"
+
+function AuthForm() {
+  return (
+    <div>AuthForm</div>
+  )
+}
+
+export default AuthForm
